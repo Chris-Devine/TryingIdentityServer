e0b1cf5dfd69ef720d3e265b218d8dfdcee4ab04
Chris Devine <Chris-Devine>
2017-09-10 00:54:37 +0100

    Protecting an API using Client Credentials

---
+++ Api1/Controllers/IdentityController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api1.Controllers
+{
+	[Route("identity")]
+	[Authorize]
+	public class IdentityController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+		}
+	}
+}
--- Api1/Program.cs
+++ Api1/Program.cs
@@ -1,18 +1,19 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using System;
 using System.IO;
 
-namespace IdentityServerBasic
+namespace Api1
 {
 	public class Program
 	{
 		public static void Main(string[] args)
 		{
-			Console.Title = "IdentityServer";
+			Console.Title = "API 1";
 
 			var host = new WebHostBuilder()
 					.UseKestrel()
-					.UseUrls("http://localhost:5000")
+					.UseUrls("http://localhost:5001")
 					.UseContentRoot(Directory.GetCurrentDirectory())
 					.UseIISIntegration()
 					.UseStartup<Startup>()
